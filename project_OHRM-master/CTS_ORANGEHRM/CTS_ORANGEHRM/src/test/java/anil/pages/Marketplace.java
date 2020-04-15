e48ec05103641c739c5cd83659e2c3802e8d35db
HP <HP@LAPTOP-20C4Q6IO>
2020-04-15 19:03:14 +0530

    Final

---
+++ project_OHRM-master/CTS_ORANGEHRM/CTS_ORANGEHRM/src/test/java/anil/pages/Marketplace.java
@@ -0,0 +1,26 @@
+package anil.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Marketplace {
+  WebDriver dr;
+  By MarketplaceTab = By.id("MP_link");
+  public Marketplace(WebDriver dr) {
+		this.dr=dr;
+	}
+
+    public void MarketplaceTab() {
+    	dr.findElement(MarketplaceTab).click();
+    }
+    public void request_Addons() {
+    	dr.findElement(By.id("buyBtn1")).click();
+    }
+    	public void enter_data() {
+        dr.findElement(By.id("email")).click();
+    	dr.findElement(By.id("email")).sendKeys("abc1@gmail.com");
+    	dr.findElement(By.id("contactNumber")).sendKeys("1234567890");
+    	dr.findElement(By.id("organization")).sendKeys("xyzorganization");
+    	dr.findElement(By.id("modal_confirm_buy")).click();
+    }	
+}
